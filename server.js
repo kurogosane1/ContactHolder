1781392ada64d0c9e8068cea167e54e4cb5e4e18
Syed Khurshid <syedkhurshid@Syeds-MBP.lan>
2019-07-10 00:19:51 -0500

    added transitions

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+//Connect Datebase
+connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +15,9 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use('/api/users', require('./Routes/users'));
 app.use('/api/auth', require('./Routes/auth'));
 app.use('/api/contacts', require('./Routes/contacts'));
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
 	res.send({ msg: 'Welcome to the contact API' });
