8588d6d6e02f50308127e009927cf6b4067ee9b6
Syed Khurshid <syedkhurshid@Syeds-MBP.lan>
2019-07-16 01:22:10 -0500

    Prepare for deployment

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
 const connectDB = require('./config/db');
+const path = require('path');
 
 //Connect Datebase
 connectDB();
@@ -19,8 +20,17 @@ app.use('/api/contacts', require('./Routes/contacts'));
 //Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-	res.send({ msg: 'Welcome to the contact API' });
-});
+// app.get('/', (req, res) => {
+// 	res.send({ msg: 'Welcome to the contact API' });
+// });
+
+//Serve Static Assets in production
+if (process.env.NODE_ENV === 'production') {
+	//Set Static Folder
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
 
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
