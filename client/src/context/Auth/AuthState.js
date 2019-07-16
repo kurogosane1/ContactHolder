0d4095e21fa09ce135faad70d75c3cd963ab102c
Syed Khurshid <syedkhurshid@Syeds-MBP.lan>
2019-07-16 01:11:19 -0500

    Completed Application

--- client/src/context/Auth/AuthState.js
+++ client/src/context/Auth/AuthState.js
@@ -88,7 +88,10 @@ const AuthState = props => {
 	};
 
 	//Logout
-	const logout = () => console.log('logout');
+	const logout = () =>
+		dispatch({
+			type: LOGOUT,
+		});
 	//Clear Errors
 	const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
 
